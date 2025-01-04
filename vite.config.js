02c93b32f32c79fae2fbc4e7b36af6ec1e746bb9
Junaid Memon <junaid.memon123@hotmail.com>
2025-01-03 18:26:44 -0700

    npm changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Portfolio/', // Replace with your repository name
   plugins: [react()],
 })
